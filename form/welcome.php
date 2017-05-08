ec6347810085938f7d7babd453e692d25d7d12f8
silenceliux <3700883007@qq.com>
2017-05-08 16:54:02 +0800

    form

---
+++ form/welcome.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<?php
+        //本程序用于接收来自HTML页面的表单数据，并输出每个字段
+        echo "用户的输入如下所示：<BR>";
+        echo "姓名：".$_POST['name']."<BR>";
+        echo "密码：".$_POST['email']."<BR>";
+        ?>
+
+</body>
+</html>
