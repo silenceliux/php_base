64d2e31124d08a501e3a4928007b188adbc787f0
silenceliux <3700883007@qq.com>
2017-05-10 15:37:02 +0800

    mysql

---
+++ mysql/mysql.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+        //主机名
+        $db_host = 'localhost';
+        //用户名
+        $db_user = 'root';
+        //密码
+        $db_password = '';
+        //数据库名
+        $db_name = 'test';
+
+        //连接数据库
+        $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name) or die('连接数据库失败！');
+
+        //选择数据库
+        //mysqli_select_db($conn, $db_name) or die('选择数据库失败！');
+
+        //查询数据库
+        $result = mysqli_query($conn, "SELECT * FROM persons");
+        //处理返回的数据集
+        $data = [];
+        while($row = mysqli_fetch_assoc($result)) {//mysqli_fetch_array
+        $data[] = $row;
+        }
+
+        //在页面上打印
+        var_dump($data);
+?>
+</body>
+</html>
