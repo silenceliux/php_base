13fac9f70b24be1ba4fbfd4766e1aecd357a0740
silenceliux <370088307@qq.com>
2017-06-16 15:15:10 +0800

    array

--- interface/b.php
+++ interface/b.php
@@ -1,3 +1,5 @@
 <?php
-        exit(json_encode("success"));
+       // $output = array('data'=>NULL, 'info'=>'The uid is null!', 'code'=>-401);//返回的是对象
+        $output = array(1,"women",3,4);//返回的是数组
+        exit(json_encode($output));
         ?>
