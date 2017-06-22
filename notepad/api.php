a20be828f7e3d7a429eb4329fa9b7435c8f2917e
silenceliux <370088307@qq.com>
2017-06-22 17:33:28 +0800

    utf8

--- notepad/api.php
+++ notepad/api.php
@@ -1,5 +1,6 @@
-
+﻿
 <?php
+        header("charset=utf-8");
         //主机名
         $db_host = 'localhost';
         //用户名
@@ -11,7 +12,9 @@
 
         //连接数据库
         $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name) or die('连接数据库失败！');
-
+         mysqli_query($conn,"SET NAMES 'UTF8'");
+        mysqli_query($conn,"SET CHARACTER SET UTF8");
+         mysqli_query($conn,"SET CHARACTER_SET_RESULTS=UTF8'");
         if($_POST['name'] == "add"){
             mysqli_query($conn, "insert into list (content) values(".$_POST['content'].")");
         }
@@ -26,7 +29,7 @@
             exit(json_encode($data));
         }
         if($_POST['name'] == "remove"){
-            mysqli_query($conn, "delete from list where id = ".$_GET['id']);
+            mysqli_query($conn, "delete from list where id = ".$_POST['id']);
         }
         if($_POST['name'] == "modify"){
         mysqli_query($conn, "update list set content='".$_POST['content']."' where id=".$_POST['id']);
